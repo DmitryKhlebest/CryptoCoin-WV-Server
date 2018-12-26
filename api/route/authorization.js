1a48f8303910459ecda4cb006b4d3c6647afdbce
Dzmitry Khlebest <d.khlebest@hackspace.by>
2018-12-26 18:20:28 +0300

    Commit

--- api/route/authorization.js
+++ api/route/authorization.js
@@ -20,7 +20,7 @@ const authorization = async (data, ws) => {
 		token = await storageSessions.addSession(user.id);
 	}
 	else
-		throw new Error("Error: token or/and login or/and password not found!");
+		throw new Error("Error: login or/and password not found!");
 
 	ws.token = token;
 
@@ -29,26 +29,6 @@ const authorization = async (data, ws) => {
 	delete user.salt;
 	delete user.hashedPassword;
 	delete user.imagePath;
-
-	// console.log(user);
-	// const imagePath = path.join(__dirname, '..', '..', user.imagePath);
-	// console.log(imagePath);
-	// // /home/kda/Study/CryptoCoin-WV/CryptoCoin-WV-Server/api/route/../../resourse/images/defaultAvatar.png
-	// const imageType = imagePath.match(/[a-z]+$/)[0];
-	// const imageName = imagePath.match(/(\w+).[a-z]+$/)[1];
-	// console.log(imageType);
-	// console.log(imageName);
-
-	// const bitmap = fs.readFileSync(imagePath);
-	// const base64 = new Buffer(bitmap).toString('base64');
-
-	// const image = `data:image/${imageType};base64,${base64}`;
-	// console.log(image);
-
-	// user.avatar = {
-	// 	image,
-	// 	name: imageName
-	// };
 
 	return {
 		ok: {
